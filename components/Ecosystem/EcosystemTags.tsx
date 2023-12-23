68d2aa543d06277ec7d353e2be4469861273fcf1
Kenk <kenghin_lim@hotmail.com>
2023-12-23 17:27:12 +0700

    refactor: use generic Tag

--- components/Ecosystem/EcosystemTags.tsx
+++ components/Ecosystem/EcosystemTags.tsx
@@ -1,180 +1,59 @@
-function EcosystemTags({ tags }) {
-    // | "bridge"
-    // | "dashboard"
-    // | "defi"
-    // | "explorer"
-    // | "gaming"
-    // | "nft"
-    // | "oracle"
-    // | "wallet"
-    // | "zk"
-    function getTag(tag) {
-        switch (tag) {
-            case "bridge":
-                return <BridgeTag />;
-            case "dashboard":
-                return <DashboardTag />;
-            case "defi":
-                return <DefiTag />;
-            case "explorer":
-                return <ExplorerTag />;
-            case "gaming":
-                return <GamingTag />;
-            case "nft":
-                return <NftTag />;
-            case "oracle":
-                return <OracleTag />;
-            case "wallet":
-                return <WalletTag />;
-            case "zk":
-                return <ZkTag />;
-            case "a3":
-                return <TestnetA3Tag />;
-            case "a5":
-                return <TestnetA5Tag />;
-            case "a6":
-                return <TestnetA6Tag />;
-            case "coming-soon":
-                return <ComingSoonTag />;
-            case "live":
-                return <LiveTag />;
-            case "enabler":
-                return <EnablerTag />;
-            default:
-                return <DefaultTag text={tag} />;
-        }
-    }
+import type { Tag } from "./EcosystemSection";
 
+type TagDataValue = {
+    color: string;
+    additionalStyles?: string;
+};
+
+type TagData = {
+    [K in Tag]: TagDataValue;
+};
+
+// Example usage
+const tagData: TagData = {
+    bridge: { color: "blue-500" },
+    dashboard: { color: "yellow-500" },
+    defi: { color: "red-500" },
+    explorer: { color: "purple-500" },
+    gaming: { color: "green-500" },
+    nft: { color: "orange-600" },
+    oracle: { color: "indigo-500" },
+    wallet: { color: "rose-500" },
+    zk: { color: "neutral-500" },
+    enabler: { color: "neutral-500" },
+    a3: { color: "[#E81899]" },
+    a5: { color: "[#E81899]", additionalStyles: "glow-pulse" },
+    a6: { color: "[#E81899]" },
+    "coming-soon": { color: "neutral-500" },
+    all: { color: "green-500" },
+};
+
+function EcosystemTags({ tags }) {
     // For each tag, render a tag component
     return (
         <div className="flex flex-wrap gap-2">
-            {tags.map((tag) => getTag(tag))}
+            {tags.map((tag) => {
+                const { color, additionalStyles = "" } = tagData[tag] || {
+                    color: "neutral-500",
+                };
+                return (
+                    <Tag
+                        key={tag}
+                        color={color}
+                        text={tag}
+                        additionalStyles={additionalStyles}
+                    />
+                );
+            })}
         </div>
     );
 }
 
-function LiveTag() {
+// Generic Tag Component
+function Tag({ color, text, additionalStyles }) {
     return (
-        <div className="bg-green-500 text-white text-xs font-bold px-2 py-1 rounded-md mb-4">
-            Live on testnet
-        </div>
-    );
-}
-
-function ComingSoonTag() {
-    return (
-        <div className="bg-neutral-500 text-white text-xs font-bold px-2 py-1 rounded-md mb-4">
-            Coming soon
-        </div>
-    );
-}
-
-function TestnetA3Tag() {
-    return (
-        <div className="bg-[#e97ec0] text-white text-xs font-bold px-2 py-1 rounded-md mb-4">
-            Testnet A3
-        </div>
-    );
-}
-
-function TestnetA5Tag() {
-    return (
-        <div className="bg-[#E81899] text-white text-xs font-bold px-2 py-1 rounded-md mb-4 glow-pulse">
-            Testnet A5
-        </div>
-    );
-}
-
-function TestnetA6Tag() {
-    return (
-        <div className="bg-[#E81899] text-white text-xs font-bold px-2 py-1 rounded-md mb-4">
-            Testnet A6
-        </div>
-    );
-}
-
-function BridgeTag() {
-    return (
-        <div className="bg-blue-500 text-white text-xs font-bold px-2 py-1 rounded-md mb-4">
-            Bridge
-        </div>
-    );
-}
-
-function DashboardTag() {
-    return (
-        <div className="bg-yellow-500 text-white text-xs font-bold px-2 py-1 rounded-md mb-4">
-            Dashboard
-        </div>
-    );
-}
-
-function DefiTag() {
-    return (
-        <div className="bg-red-500 text-white text-xs font-bold px-2 py-1 rounded-md mb-4">
-            DeFi
-        </div>
-    );
-}
-
-function ExplorerTag() {
-    return (
-        <div className="bg-purple-500 text-white text-xs font-bold px-2 py-1 rounded-md mb-4">
-            Explorer
-        </div>
-    );
-}
-
-function GamingTag() {
-    return (
-        <div className="bg-green-500 text-white text-xs font-bold px-2 py-1 rounded-md mb-4">
-            Gaming
-        </div>
-    );
-}
-
-function NftTag() {
-    return (
-        <div className="bg-orange-600 text-white text-xs font-bold px-2 py-1 rounded-md mb-4">
-            NFT
-        </div>
-    );
-}
-
-function OracleTag() {
-    return (
-        <div className="bg-indigo-500 text-white text-xs font-bold px-2 py-1 rounded-md mb-4">
-            Oracle
-        </div>
-    );
-}
-
-function WalletTag() {
-    return (
-        <div className="bg-rose-500 text-white text-xs font-bold px-2 py-1 rounded-md mb-4">
-            Wallet
-        </div>
-    );
-}
-
-function ZkTag() {
-    return (
-        <div className="bg-neutral-500 text-white text-xs font-bold px-2 py-1 rounded-md mb-4">
-            ZK
-        </div>
-    );
-}
-
-function EnablerTag() {
-    return (
-        <div className="text-white text-xs font-bold px-2 py-1 rounded-md mb-4 silver-gradient">
-            Enabler
-        </div>
-    );
-}
-function DefaultTag({ text }) {
-    return (
-        <div className="bg-neutral-500 text-white text-xs font-bold px-2 py-1 rounded-md mb-4">
+        <div
+            className={`bg-${color} text-white text-xs font-bold px-2 py-1 rounded-md mb-4 ${additionalStyles}`}>
             {text}
         </div>
     );
