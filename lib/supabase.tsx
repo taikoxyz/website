b303861134910fd82dc0368db7af48010c1be1d6
Kenk <kenghin_lim@hotmail.com>
2023-12-23 17:09:41 +0700

    feat: input validation for prover endpoints

--- lib/supabase.tsx
+++ lib/supabase.tsx
@@ -2,94 +2,136 @@ import type { Database } from "../database.types";
 import { createClient } from "@supabase/supabase-js";
 
 export const client = createClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""
+  process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""
 );
 
 type Endpoint = {
-    user_id?: string;
-    prover_name: string;
-    prover_url: string;
-    prover_fee: number;
+  user_id?: string;
+  prover_name: string;
+  prover_url: string;
+  prover_fee: number;
+};
+
+type InputValiationResponse = {
+  result: boolean;
+  error?: string;
+};
+
+type AddOrEditEndpointResult = {
+  validation_error?: string;
+  data?: any;
+  error?: any;
 };
 
 export async function getEndpoints() {
-    const { data, error } = await client.from("prover_market").select("*");
+  const { data, error } = await client.from("prover_market").select("*");
 
-    return data;
+  return data;
 }
 
-export async function addEndpoint(endpoint: Endpoint) {
-    let session = await client.auth.getSession();
+function endpointInputValidation(endpoint: Endpoint): InputValiationResponse {
+  if (!endpoint.prover_name) {
+    return { result: false, error: "Prover name is required" };
+  }
 
-    if (!session) {
-        console.error("User must be logged in to insert data");
-        return;
-    }
+  // Add the user_id to the endpoint data
+  if (endpoint.prover_name.toLowerCase().includes("taiko")) {
+    return { result: false, error: "Name cannot include Taiko." };
+  }
 
-    // Add the user_id to the endpoint data
-    if (endpoint.prover_name.toLowerCase().includes("taiko")) {
-        console.error("Name cannot include Taiko.");
-        return;
-    }
+  if (!endpoint.prover_url) {
+    return { result: false, error: "Prover URL is required" };
+  }
 
+  if (!endpoint.prover_fee) {
+    return { result: false, error: "Prover fee is required" };
+  }
+
+  // Prover URL must be a valid URL
+  try {
+    new URL(endpoint.prover_url);
+  } catch (error) {
+    return { result: false, error: "Prover URL is invalid" };
+  }
+
+  return { result: true };
+}
+
+export async function addEndpoint(
+  endpoint: Endpoint
+): Promise<AddOrEditEndpointResult> {
+  let session = await client.auth.getSession();
+
+  if (!session) {
+    console.error("User must be logged in to insert data");
+    return;
+  }
+
+  // Input validation
+  const { result, error: validation_error } = endpointInputValidation(endpoint);
+
+  if (!result) {
+    return { validation_error };
+  } else {
     const endpointWithUserId = {
-        ...endpoint,
-        user_id: session.data.session.user.id,
+      ...endpoint,
+      user_id: session.data.session.user.id,
     };
 
-    const { data, error } = await client
-        .from("prover_market")
-        .insert([endpointWithUserId]);
+    const { error } = await client
+      .from("prover_market")
+      .insert([endpointWithUserId]);
 
-    return data;
+    return { error };
+  }
 }
 /**
  * Edit endpoint in database (Must be authorized, logged into github)
  */
-export async function editEndpoint(endpoint: Endpoint) {
-    let session = await client.auth.getSession();
+export async function editEndpoint(
+  endpoint: Endpoint
+): Promise<AddOrEditEndpointResult> {
+  let session = await client.auth.getSession();
 
-    if (!session) {
-        console.error("User must be logged in to insert data");
-        return;
-    }
+  if (!session) {
+    console.error("User must be logged in to insert data");
+    return;
+  }
 
-    if (endpoint.prover_name.toLowerCase().includes("taiko")) {
-        console.error("Name cannot include Taiko.");
-        return;
-    }
+  // Input validation
+  const { result, error: validation_error } = endpointInputValidation(endpoint);
 
-    const { data, error } = await client
-        .from("prover_market")
-        .update(endpoint)
-        .match({ user_id: session.data.session.user.id });
+  if (!result) {
+    return { validation_error };
+  } else {
+    const { error } = await client
+      .from("prover_market")
+      .update(endpoint)
+      .match({ user_id: session.data.session.user.id });
 
-    if (error) {
-        return error;
-    }
-
-    return data;
+    return { error };
+  }
 }
 
 export async function removeEndpoint(endpoint: Endpoint) {
-    let session = await client.auth.getSession();
+  let session = await client.auth.getSession();
 
-    if (!session) {
-        console.error("User must be logged in to delete data");
-        return;
-    }
+  if (!session) {
+    console.error("User must be logged in to delete data");
+    return;
+  }
 
-    const { error } = await client.from("prover_market").delete().match({
-        user_id: endpoint.user_id,
-        prover_name: endpoint.prover_name,
-        prover_url: endpoint.prover_url,
-        prover_fee: endpoint.prover_fee,
-    });
-    if (error) {
-        console.error("Error deleting endpoint:", error);
-        return;
-    }
+  const { error } = await client.from("prover_market").delete().match({
+    user_id: endpoint.user_id,
+    prover_name: endpoint.prover_name,
+    prover_url: endpoint.prover_url,
+    prover_fee: endpoint.prover_fee,
+  });
+  if (error) {
+    console.error("Error deleting endpoint:", error);
+    return;
+  }
 
-    console.log("Endpoint deleted successfully");
+  console.log("Endpoint deleted successfully");
 }
